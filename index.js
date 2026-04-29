686358bd87c69d24c332a2c99bc4852d8bb7862a
steeven1329 <33669281+steeven1329@users.noreply.github.com>
2026-04-29 10:25:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Youumii backend is running 🚀');
+// Connexion à Supabase
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+// Route principale
+app.get('/', async (req, res) => {
+  const { data, error } = await supabase
+    .from('test')
+    .select('*');
+
+  if (error) {
+    return res.send("Erreur: " + error.message);
+  }
+
+  res.json(data);
 });
 
+// Santé
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
